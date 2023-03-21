d320bdcc9fd4a89e980abfb027a848166a87d7fc
shiiiniii <115028625+shiiiniii@users.noreply.github.com>
2023-03-21 07:40:46 +0300

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,13 @@
+package finalwork;
+
+public class Menu {
+    public static void print(){
+        System.out.println("Введите номер параметра, который ищите: \n" +
+                "\t1 - ОЗУ\n" +
+                "\t2 - Объем ЖД\n" +
+                "\t3 - Операционная система\n" +
+                "\t4 - Цвет \n" +
+                "\t0 - Выход");
+        System.out.print("=> ");
+    }
+}
